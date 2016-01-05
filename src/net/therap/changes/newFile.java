e7f16aed0b0c1b6df9b5efa7f0b62e74c5195d73
Md. Rafsan Jani <rafsan.jani@therapservices.net>
2016-01-05 13:38:34 +0600

    new file added and rebased later

---
+++ src/net/therap/changes/newFile.java
@@ -0,0 +1,11 @@
+package net.therap.changes;
+
+/**
+ * @author: rafsan.jani
+ * @since: 1/5/16.
+ */
+public class newFile {
+    public newFile(){
+        System.out.println("New File created..");
+    }
+}
